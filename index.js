99b0120eedba35f166e28598487e1485b32bebfd
MrFranksJr <112629948+MrFranksJr@users.noreply.github.com>
2022-11-05 11:02:37 +0100

    Committing recent changes

--- index.js
+++ index.js
@@ -1,229 +1,42 @@
-const catsData = [
-    {
-        emotionTags: ["moody"],
-        isGif: false,
-        image: "angry.jpeg",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["moody", "insomniac"],
-        isGif: false,
-        image: "angry2.jpeg",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["moody"],
-        isGif: false,
-        image: "angry3.jpeg",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["confused", "sad"],
-        isGif: false,
-        image: "confused.jpeg",
-        alt: "A cat looking confused",
-    },
-    {
-        emotionTags: ["dominant", "moody"],
-        isGif: false,
-        image: "dominant.jpeg",
-        alt: "A cat looking dominant",
-    },
-    {
-        emotionTags: ["happy", "relaxed"],
-        isGif: false,
-        image: "happy.jpeg",
-        alt: "A cat looking happy",
-    },
-    {
-        emotionTags: ["hungry"],
-        isGif: false,
-        image: "hungry.jpeg",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["hungry"],
-        isGif: false,
-        image: "hungry1.jpeg",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["insomniac"],
-        isGif: false,
-        image: "insomnia.jpeg",
-        alt: "A cat looking insomniac",
-    },
-    {
-        emotionTags: ["insomniac"],
-        isGif: false,
-        image: "insomnia1.jpeg",
-        alt: "A cat looking insomniac",
-    },
-    {
-        emotionTags: ["relaxed"],
-        isGif: false,
-        image: "lazy.jpeg",
-        alt: "A cat looking lazy",
-    },
-    {
-        emotionTags: ["scared"],
-        isGif: false,
-        image: "nervous.jpeg",
-        alt: "A cat looking nervous",
-    },
-    {
-        emotionTags: ["sad"],
-        isGif: false,
-        image: "sad.jpeg",
-        alt: "A cat looking sad",
-    },
-    {
-        emotionTags: ["sad", "moody"],
-        isGif: false,
-        image: "sad1.jpeg",
-        alt: "A cat looking sad",
-    },
-    {
-        emotionTags: ["moody"],
-        isGif: true,
-        image: "angry.gif",
-        alt: "A cat looking moody",
-    },
-    {
-        emotionTags: ["moody"],
-        isGif: true,
-        image: "angry2.gif",
-        alt: "A cat looking angry",
-    },
-    {
-        emotionTags: ["confused"],
-        isGif: true,
-        image: "confused2.gif",
-        alt: "A cat looking confused",
-    },
-    {
-        emotionTags: ["dominant"],
-        isGif: true,
-        image: "dominant.gif",
-        alt: "A cat looking dominant",
-    },
-    {
-        emotionTags: ["happy"],
-        isGif: true,
-        image: "happy.gif",
-        alt: "A cat looking happy",
-    },
-    {
-        emotionTags: ["hungry", "sad", "confused"],
-        isGif: true,
-        image: "confused.gif",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["hungry"],
-        isGif: true,
-        image: "hungry.gif",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["hungry"],
-        isGif: true,
-        image: "hungry2.gif",
-        alt: "A cat looking hungry",
-    },
-    {
-        emotionTags: ["insomniac", "scared"],
-        isGif: true,
-        image: "insomnia2.gif",
-        alt: "A cat looking insomniac",
-    },
-    {
-        emotionTags: ["relaxed"],
-        isGif: true,
-        image: "lazy.gif",
-        alt: "A cat looking relaxed",
-    },
-    {
-        emotionTags: ["relaxed"],
-        isGif: true,
-        image: "relaxed2.gif",
-        alt: "A cat looking relaxed",
-    },
-    {
-        emotionTags: ["scared", "sad"],
-        isGif: true,
-        image: "nervous.gif",
-        alt: "A cat looking nervous",
-    },
-    {
-        emotionTags: ["scared"],
-        isGif: true,
-        image: "nervous2.gif",
-        alt: "A cat looking scared",
-    },
-    {
-        emotionTags: ["sad"],
-        isGif: true,
-        image: "sad.gif",
-        alt: "A cat looking sad",
-    },
-]
-const emotionRadiosDiv = document.getElementById("emotion-radios")
+import { catsData } from '/data.js'
+const emotionRadios = document.getElementById("emotion-radios")
 const getImageBtn = document.getElementById("get-image-btn")
 
+emotionRadios.addEventListener('change', function (e) {
+    let selectedRadio = document.getElementById(e.target.id).parentElement
+    let allHighlights = document.getElementsByClassName('highlight')
+    for (let element of allHighlights) {
+        element.classList.toggle('highlight')
+    }
+    selectedRadio.classList.toggle('highlight')
+})
+
+
 //get all emotions from the cats array into an emotions array
-function getEmotionsArray(cats){
+function getEmotionsArray(cats) {
     const allEmotions = []
     for (let cat of cats) {
         for (let emotion of cat.emotionTags) {
             allEmotions.push(emotion)
         }
     }
-    return allEmotions
+    const uniqueEmotions = [...new Set(allEmotions)];
+    return uniqueEmotions
 }
 
 //get emotionsarray and return radios
-function renderEmotionsArray(cats) {
+function renderEmotionsRadios(cats) {
     const emotions = getEmotionsArray(cats)
     let htmlString = ""
     for (let emotion of emotions) {
-        htmlString += `<p>${emotion}</p>`
+        htmlString += `
+                        <div class="radio">
+                        <label for="${emotion}">${emotion}</label>
+                        <input type="radio" id="${emotion}" value="${emotion}" name="emotion-radios">
+                        </div>
+                        `
     }
-    emotionRadiosDiv.innerHTML = htmlString
+    emotionRadios.innerHTML = htmlString
 }
 
-renderEmotionsArray(catsData)
-
-
-
-
-
-
-
-//darkmode
-const dModeToggle = document.getElementById("darkmode-toggle")
-const bodyEl = document.body
-const headEr = document.getElementById("header")
-
-
-dModeToggle.addEventListener("change", (event) => {
-    if (event.currentTarget.checked) {
-        switchModes()
-    } else {
-        switchModes()
-    }
-  })
-  
-  function switchModes() {
-    if (dModeToggle.checked) {
-        bodyEl.classList.add("darkmode")
-        headEr.classList.add("darkmodeHeader")
-        getImageBtn.classList.add("dModeButton")
-    }
-    else {
-        bodyEl.classList.remove("darkmode")
-        headEr.classList.remove("darkmodeHeader")
-        getImageBtn.classList.remove("dModeButton")
-    }
-  }
-  switchModes()
+renderEmotionsRadios(catsData)
